d0a6d04666c0463b98af42f0f1034fae6fb7d023
weixin <120426714@qq.com>
2026-04-30 16:49:44 +0800

    feat: configure app shell

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,37 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/home/index',
+    'pages/test-list/index',
+    'pages/test-detail/index',
+    'pages/quiz/index',
+    'pages/result/index',
+    'pages/profile/index'
   ],
   window: {
     backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
+    navigationBarBackgroundColor: '#f7efe2',
+    navigationBarTitleText: '心测研究所',
+    navigationBarTextStyle: 'black',
+    backgroundColor: '#f7efe2'
+  },
+  tabBar: {
+    color: '#6b665b',
+    selectedColor: '#275241',
+    backgroundColor: '#fffaf0',
+    borderStyle: 'white',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '首页'
+      },
+      {
+        pagePath: 'pages/test-list/index',
+        text: '测试'
+      },
+      {
+        pagePath: 'pages/profile/index',
+        text: '我的'
+      }
+    ]
   }
 })
